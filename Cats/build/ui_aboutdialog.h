6f2f1797f99d4ec4b150b9e267796168ecb3326d
cats master <92247740+BT1496@users.noreply.github.com>
2021-11-29 16:08:11 +0900

    Add files via upload

---
+++ Cats/build/ui_aboutdialog.h
@@ -0,0 +1,166 @@
+/********************************************************************************
+** Form generated from reading UI file 'aboutdialog.ui'
+**
+** Created by: Qt User Interface Compiler version 4.8.7
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ABOUTDIALOG_H
+#define UI_ABOUTDIALOG_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QDialog>
+#include <QtGui/QDialogButtonBox>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QLabel>
+#include <QtGui/QSpacerItem>
+#include <QtGui/QVBoxLayout>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_AboutDialog
+{
+public:
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label_4;
+    QVBoxLayout *verticalLayout_2;
+    QSpacerItem *verticalSpacer_2;
+    QHBoxLayout *horizontalLayout;
+    QLabel *label;
+    QLabel *versionLabel;
+    QSpacerItem *horizontalSpacer;
+    QLabel *copyrightLabel;
+    QLabel *label_2;
+    QSpacerItem *verticalSpacer;
+    QDialogButtonBox *buttonBox;
+
+    void setupUi(QDialog *AboutDialog)
+    {
+        if (AboutDialog->objectName().isEmpty())
+            AboutDialog->setObjectName(QString::fromUtf8("AboutDialog"));
+        AboutDialog->resize(747, 360);
+        horizontalLayout_2 = new QHBoxLayout(AboutDialog);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        label_4 = new QLabel(AboutDialog);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Ignored);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(label_4->sizePolicy().hasHeightForWidth());
+        label_4->setSizePolicy(sizePolicy);
+        label_4->setPixmap(QPixmap(QString::fromUtf8(":/images/about")));
+
+        horizontalLayout_2->addWidget(label_4);
+
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalSpacer_2 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_2->addItem(verticalSpacer_2);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        label = new QLabel(AboutDialog);
+        label->setObjectName(QString::fromUtf8("label"));
+        QFont font;
+        font.setFamily(QString::fromUtf8("Manjari"));
+        label->setFont(font);
+        label->setCursor(QCursor(Qt::IBeamCursor));
+        label->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+        horizontalLayout->addWidget(label);
+
+        versionLabel = new QLabel(AboutDialog);
+        versionLabel->setObjectName(QString::fromUtf8("versionLabel"));
+        versionLabel->setFont(font);
+        versionLabel->setCursor(QCursor(Qt::IBeamCursor));
+        versionLabel->setText(QString::fromUtf8("0.1-beta"));
+        versionLabel->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+        horizontalLayout->addWidget(versionLabel);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer);
+
+
+        verticalLayout_2->addLayout(horizontalLayout);
+
+        copyrightLabel = new QLabel(AboutDialog);
+        copyrightLabel->setObjectName(QString::fromUtf8("copyrightLabel"));
+        QFont font1;
+        font1.setFamily(QString::fromUtf8("Manjari"));
+        font1.setPointSize(12);
+        font1.setItalic(false);
+        copyrightLabel->setFont(font1);
+        copyrightLabel->setCursor(QCursor(Qt::IBeamCursor));
+        copyrightLabel->setLineWidth(1);
+        copyrightLabel->setText(QString::fromUtf8(" &copy; 2022-YYYY The Cats developers"));
+        copyrightLabel->setTextFormat(Qt::RichText);
+        copyrightLabel->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+        verticalLayout_2->addWidget(copyrightLabel);
+
+        label_2 = new QLabel(AboutDialog);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setFont(font);
+        label_2->setCursor(QCursor(Qt::IBeamCursor));
+        label_2->setWordWrap(true);
+        label_2->setTextInteractionFlags(Qt::LinksAccessibleByMouse|Qt::TextSelectableByKeyboard|Qt::TextSelectableByMouse);
+
+        verticalLayout_2->addWidget(label_2);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_2->addItem(verticalSpacer);
+
+        buttonBox = new QDialogButtonBox(AboutDialog);
+        buttonBox->setObjectName(QString::fromUtf8("buttonBox"));
+        QFont font2;
+        font2.setFamily(QString::fromUtf8("Manjari"));
+        font2.setBold(false);
+        font2.setItalic(false);
+        font2.setWeight(50);
+        buttonBox->setFont(font2);
+        buttonBox->setOrientation(Qt::Horizontal);
+        buttonBox->setStandardButtons(QDialogButtonBox::Ok);
+
+        verticalLayout_2->addWidget(buttonBox);
+
+
+        horizontalLayout_2->addLayout(verticalLayout_2);
+
+
+        retranslateUi(AboutDialog);
+        QObject::connect(buttonBox, SIGNAL(accepted()), AboutDialog, SLOT(accept()));
+        QObject::connect(buttonBox, SIGNAL(rejected()), AboutDialog, SLOT(reject()));
+
+        QMetaObject::connectSlotsByName(AboutDialog);
+    } // setupUi
+
+    void retranslateUi(QDialog *AboutDialog)
+    {
+        AboutDialog->setWindowTitle(QApplication::translate("AboutDialog", "About Cats", 0, QApplication::UnicodeUTF8));
+        label->setText(QApplication::translate("AboutDialog", "<html><head/><body><p><span style=\" font-size:12pt; font-weight:600;\">Cats</span><span style=\" font-size:12pt;\"> version</span></p></body></html>", 0, QApplication::UnicodeUTF8));
+        label_2->setText(QApplication::translate("AboutDialog", "\n"
+"This is experimental software.\n"
+"\n"
+"Distributed under the MIT/X11 software license, see the accompanying file COPYING or http://www.opensource.org/licenses/mit-license.php.\n"
+"\n"
+"This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (http://www.openssl.org/) and cryptographic software written by Eric Young (eay@cryptsoft.com) and UPnP software written by Thomas Bernard.", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class AboutDialog: public Ui_AboutDialog {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ABOUTDIALOG_H
